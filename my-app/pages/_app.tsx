3feaa98f52b3f7afa925575357abe78765c71b65
Andrew <andrew@ripemetrics.com>
2024-04-08 12:59:23 -0700

    Refactor _app.tsx, PokemonDetails.tsx, index.tsx, and [name]/index.tsx files

--- my-app/pages/_app.tsx
+++ my-app/pages/_app.tsx
@@ -1,6 +1,29 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Provider } from "react-redux";
+import { store } from "./store/store";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Provider store={store}>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          flexDirection: "column",
+          height: "98vh",
+          width: "98vw",
+          border: "1px solid black",
+          boxShadow: "0 10 10px rgba(77, 77, 77, 0.589)",
+          borderRadius: 10,
+          backgroundColor: "white",
+          color: "black",
+          overflow: "auto",
+        }}
+      >
+        <Component {...pageProps} />
+      </div>
+    </Provider>
+  );
 }
